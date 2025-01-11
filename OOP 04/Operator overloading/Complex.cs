413d0da23413be277d42ac5c6eee5739c5a37a62
Mohamed Atef <Mohamed Atef>
2025-01-11 14:41:14 +0200

    Operator Overloading and Uniery Operator

---
+++ OOP 04/Operator overloading/Complex.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_04.Operator_overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+
+        public int Imag { get; set; }
+
+        // Operator Overloading
+        // +
+        // Must be : Public - static
+        public static Complex operator +(Complex left, Complex right)
+        {
+            return new Complex()
+            {
+                Real = left?.Real ?? 0+ right.Real,
+                Imag = left?.Imag ?? 0 + right.Imag
+            };
+        }
+
+
+        public static Complex operator -(Complex left, Complex right)
+        {
+            return new Complex()
+            {
+                Real = left.Real - right.Real,
+                Imag = left.Imag - right.Imag
+            };
+        }
+
+        // Operator Overloading
+        // ++ --
+        // Must be : Public - static
+        public static Complex operator ++(Complex complex)
+        {
+            complex.Real++;
+            return complex;
+        }
+
+        public static Complex operator --(Complex complex)
+        {
+            complex.Real--;
+            return complex;
+        }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag} i";
+        }
+    }
+}
--- OOP 04/Program.cs
+++ OOP 04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOP_04.Abstracts;
+using OOP_04.Operator_overloading;
 
 namespace OOP_04
 {
@@ -29,6 +30,35 @@ namespace OOP_04
             #region Interface Vs Abstract Class
             //Interface Vs Abstract Class 
             #endregion
+
+            #region Operator Overloading
+            // Operator Overloading
+
+            // *-/+||&&>>
+
+            // Complex Number : 
+
+            Complex c01 = new Complex() { Real = 3, Imag = 4 };
+            Complex c02 = new Complex() { Real = 3, Imag = 4 };
+
+            Console.WriteLine(c01);
+            Console.WriteLine(c02);
+
+            Complex C03 = c01 + c02;
+            Console.WriteLine(C03);
+
+            // ++ --
+            int x = 12;
+            x++;
+            Console.WriteLine(x);
+
+            c01++;
+            Console.WriteLine(c01);
+
+            c01--;
+            Console.WriteLine(c01); 
+            #endregion
+
         }
     }
 }
