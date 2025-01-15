a9cf0acddf421c2604820d0364fb6101fca5f858
Mohamed Atef <mohammed.atef.saif@gmail.com>
2025-01-15 16:43:09 +0200

    Third Project

--- OOP 04/Program.cs
+++ OOP 04/Program.cs
@@ -147,6 +147,7 @@ namespace OOP_04
             // Sealed : C# keyword [class - method - property]  Cant override or inheret 
             #endregion
 
+            #region Dev
             #region Firt Project
             //// First Project
             //Point3D p1 = ReadInput("P1");
@@ -209,6 +210,58 @@ namespace OOP_04
             //Console.WriteLine($"multiply: {multiply}");
 
             #endregion
+
+            #region Third Project
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1.ToString());
+
+            //Duration D2 = new Duration(7800);
+            //Console.WriteLine(D2.ToString());
+
+            //Duration D3 = new Duration(666);
+            //Console.WriteLine(D3.ToString());
+
+            //Duration D4 = new Duration(6);
+            //Console.WriteLine(D4.ToString());
+
+            //D3 = D1 + D2;
+            //Console.WriteLine(D3.ToString()); 
+
+            //D3 = D1 + 7800;
+            //Console.WriteLine(D3.ToString()); 
+
+            //D3 = 666 + D3;
+            //Console.WriteLine(D3.ToString()); 
+
+            //D3 = ++D1;
+            //Console.WriteLine(D3.ToString()); 
+
+            //D3 = --D2;
+            //Console.WriteLine(D3.ToString()); 
+
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1.ToString());
+
+            //if (D1 > D2)
+            //{
+            //   Console.WriteLine("D1 is greater than D2");
+            //}
+            //else
+            //{
+            //   Console.WriteLine("D1 is not greater than D2");
+            //}
+
+            //if (D1 <= D2)
+            //    Console.WriteLine("D1 is less than or equal D2");
+
+            //if (D1)
+            //    Console.WriteLine($"D1: {D1}");
+
+            //DateTime dateTime = (DateTime)D1;
+            //Console.WriteLine($"DateTime: {dateTime:HH:mm:ss}"); 
+            #endregion 
+            #endregion
+
 
         }
     }
