68ef1479bf6a9e788c548cd789878631e0678acd
Mohamed Atef <Mohamed Atef>
2025-01-24 20:29:41 +0200

    Generics

---
+++ Session 01/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_01
+{
+    internal static class Helper
+    {
+       public static void SWAP<T>(ref T x, ref T y)
+        {
+            T Temp = x;
+            x = y;
+            y = Temp;
+        }
+
+        //public static int LinearSearch(int[] Arr, int value)
+        //{
+        //    if (Arr?.Length > 0)
+        //    {
+        //        for (int i = 0; i < Arr.Length; i++)
+        //        {
+        //            if (Arr[i] == value) return i;
+        //        }
+        //    }
+        //    return -1;
+        //}
+
+        public static int LinearSearch<T>(T[] Arr, T value)
+        {
+            if (Arr?.Length > 0)
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    if (Arr[i].Equals(value)) return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Session 01/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Session_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generics : 
+
+            #region Non Generic SWAP
+            //int a = 1;
+            //int b = 2;
+
+            //Console.WriteLine($"a: {a}");
+            //Console.WriteLine($"b: {b}");
+            //Console.WriteLine("After swap");
+            //Helper.SWAP(ref a, ref b);
+            //Console.WriteLine($"a: {a}");
+            //Console.WriteLine($"b: {b}");
+
+            //double a = 1;
+            //double b = 2;
+
+            //Console.WriteLine($"a: {a}");
+            //Console.WriteLine($"b: {b}");
+            //Console.WriteLine("After swap");
+            //Helper.SWAP(ref a, ref b);
+            //Console.WriteLine($"a: {a}");
+            //Console.WriteLine($"b: {b}"); 
+            #endregion
+
+            #region Generic SWAP
+            //int a = 1;
+            //int b = 2;
+
+            //Console.WriteLine($"a: {a}");
+            //Console.WriteLine($"b: {b}");
+            //Console.WriteLine("After swap");
+            //Helper<int>.SWAP(ref a, ref b);
+            //Console.WriteLine($"a: {a}");
+            //Console.WriteLine($"b: {b}");
+
+            //double x = 1;
+            //double y = 2;
+
+            //Console.WriteLine($"a: {x}");
+            //Console.WriteLine($"b: {y}");
+            //Console.WriteLine("After swap");
+            //Helper<double>.SWAP(ref y, ref x);
+            //Console.WriteLine($"a: {x}");
+            //Console.WriteLine($"b: {y}"); 
+            #endregion
+
+            
+        }
+    }
+}
