4e72515f43af13b279478eeebea26291bad5977d
Mohamed Atef <Mohamed Atef>
2025-01-31 15:40:52 +0200

    Delegate EX01

--- Session 03/Program.cs
+++ Session 03/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace Session_03
 {
+    // Class
+    // Struct
+    // Enum
+    // Interface
+    // Delegate
+
+    public delegate int StringFancDelegate(string s);
+    // NEW Delegate => compiler deal with it as a class ==> Pure OOP
+    // U can create from delegate a Reference (Pointer) can refre to function or more
+    // these function must be have the same signature of the delegate
+    // Regardless function name, param name or, access modifiers
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate Overview
+            // Delegate: C# Feature
+            // C# Pure OOP
+
+            // Functional Programming
+            // Event-Driven Programming 
+            #endregion
+
+            #region Delegate EX01
+            //StringFancDelegate x;
+            //x = StringFunctions.GetCountOfUpperCaseChars;
+            //x = StringFunctions.GetCountOfLowerCaseChars;
+            ////int count = x.Invoke("Hello World");
+            //int count = x("Hello World"); // syntax sugar
+            //Console.WriteLine(count); 
+            #endregion
+
+
         }
     }
 }
