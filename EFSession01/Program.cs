b9f24dc7ab3a0396fe2f8718110a33f1f5c5dfd8
Mohamed Atef <Mohamed Atef>
2025-02-18 20:40:11 +0200

    Comparison ADO,EF Core and Dapper

---
+++ EFSession01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EFSession01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Comparison ADO,EF Core and Dapper
+            // ADO Writes more code and EF Core write less code
+            // EF Core built on top of ADO
+            // which means ADO is faster than EF Core
+            // Dapper is simple close more to ADO than EF Core 
+            #endregion
+
+
+
+        }
+    }
+}
