4e369b90b20c36bc04a4d7cedaf0a6d43addfae0
Mohamed Atef <Mohamed Atef>
2025-02-24 20:20:20 +0200

    Notes

---
+++ MVCSession01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MVCSession01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dot net is a container for technolges
+            
+            // For Web ==> ASP.NET - Blazor
+            
+            // ASP.NET ==> MVC - Razor - Web Api
+            
+            // MVC => Model View Controller
+            // Model => class represent table in database
+            // view => html page
+            // controller => project controller
+            
+            // HTTP => Hypertext Transfer Protocol
+            // TCP => Transmission control Protocol
+
+            // Status Code
+            // 100 - 199 => Information
+            // 200 - 299 => Successful
+            // 300 - 399 => Redirection
+            // 400 - 499 => Client Error
+            // 500 - 599 => Server Error
+
+
+        }
+    }
+}
